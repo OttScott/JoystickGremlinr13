382e859c6e80e6c9a01d5f891c59dcf7985e74bc
WhiteMagic <white.magic@gmx.de>
2025-12-06 10:52:23 +0100

    Slight model cleanup

--- gremlin/ui/profile_devices_model.py
+++ gremlin/ui/profile_devices_model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8; -*-
 
-# Copyright (C) 2015 - 2025 Lionel Ott
+# Copyright (C) 2025 Lionel Ott
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any
+
+from __future__ import annotations
+
+from typing import (
+    Any,
+    TYPE_CHECKING,
+)
 
 from PySide6 import (
     QtCore,
@@ -28,10 +34,11 @@ from gremlin import (
     swap_devices,
 )
 from gremlin.error import GremlinError
-from gremlin.ui import (
-    backend,
-    type_aliases,
-)
+from gremlin.ui import backend
+
+if TYPE_CHECKING:
+    import gremlin.ui.type_aliases as ta
+
 
 QML_IMPORT_NAME = "Gremlin.Tools"
 QML_IMPORT_MAJOR_VERSION = 1
@@ -39,7 +46,11 @@ QML_IMPORT_MAJOR_VERSION = 1
 
 @QtQml.QmlElement
 class ProfileDeviceListModel(QtCore.QAbstractListModel):
+
+    # TODO: Make this obsolete by augmenting the existing DeviceListModel.
+
     """Model listing devices with bindings in the profile."""
+    
     selectedIndexChanged = QtCore.Signal()
 
     roles = {
@@ -48,7 +59,7 @@ class ProfileDeviceListModel(QtCore.QAbstractListModel):
         QtCore.Qt.UserRole + 3: QtCore.QByteArray("numBindings".encode())
     }
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: ta.OQO = None) -> None:
         super().__init__(parent)
         self._selected_index = -1
         self._devices: list[swap_devices.ProfileDeviceInfo] = []
@@ -57,15 +68,16 @@ class ProfileDeviceListModel(QtCore.QAbstractListModel):
 
     def update_model(self) -> None:
         """Updates the model if the connected devices change."""
-        self._devices = swap_devices.get_profile_devices(backend.Backend().profile)
+        self._devices = \
+            swap_devices.get_profile_devices(backend.Backend().profile)
         # Ensure the entire model is refreshed
         self.modelReset.emit()
 
-    def rowCount(self, parent: type_aliases.MI = QtCore.QModelIndex()) -> int:
+    def rowCount(self, parent: ta.MI = QtCore.QModelIndex()) -> int:
         return len(self._devices)
 
     def data(
-        self, index: type_aliases.MI, role: int = QtCore.Qt.DisplayRole
+        self, index: ta.MI, role: int = QtCore.Qt.ItemDataRole.DisplayRole
     ) -> Any:
         if not index.isValid() or not (0 <= index.row() < len(self._devices)):
             return None
@@ -94,11 +106,11 @@ class ProfileDeviceListModel(QtCore.QAbstractListModel):
         return str(self._devices[index].device_uuid)
     
     @QtCore.Property(int, notify=selectedIndexChanged)
-    def selectedIndex(self):
+    def selectedIndex(self) -> int:
         return self._selected_index
     
     @selectedIndex.setter
-    def selectedIndex(self, index):
+    def selectedIndex(self, index: int) -> None:
         if 0 <= index < len(self._devices) and index != self._selected_index:
             self._selected_index = index
-        self.selectedIndexChanged.emit()
+            self.selectedIndexChanged.emit()
